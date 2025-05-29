9a1cab4234f5e035b84c5c33cd0c996764d61bcd
Volkan Şah <VolkanSah@users.noreply.github.com>
2025-05-29 23:55:05 +0200

    Create db-config.php

---
+++ the-idea/db-config.php
@@ -0,0 +1,6 @@
+<?php
+// Hardcoded database pool example, can be extended or replaced with config GUI
+return [
+    'db1' => ['host' => '127.0.0.1', 'user' => 'user1', 'pass' => 'pw1', 'db' => 'directory_db1', 'weight' => 2],
+    'db2' => ['host' => '127.0.0.2', 'user' => 'user2', 'pass' => 'pw2', 'db' => 'directory_db2', 'weight' => 1],
+];
